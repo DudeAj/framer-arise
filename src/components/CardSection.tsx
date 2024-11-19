2abd98613cf340f4fedf44102a0169db553cbc85
Ajay Kumar <ajaygangwar04@gmail.com>
2024-11-20 03:03:59 +0530

    fix columns

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -10,7 +10,7 @@ interface CardSectionProps {
 }
 
 const CardSection: React.FC<CardSectionProps> = ({
-  columns = 3,
+  columns = 2,
   features,
   gap = 8,
   titleWithIcon,
@@ -18,7 +18,7 @@ const CardSection: React.FC<CardSectionProps> = ({
 }) => {
   return (
     <section className="flex mt-10 justify-center">
-      <div className="z-10 w-10/12 self-center mt-0 max-w-[1260px] max-md:max-w-full">
+      <div className="z-10 w-10/12 self-center mt-0">
         <div className={`grid grid-cols-${columns} gap-${gap}`}>
           {features.map((feature, index) => (
             <Card titleWithIcon={titleWithIcon} key={index} {...feature} />
